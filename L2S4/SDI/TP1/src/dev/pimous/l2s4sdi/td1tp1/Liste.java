6cb28029fa0284384c5b519fa0ff011bd5c521ea
Xibitol <xibitol@pimous.dev>
2025-05-03 21:19:46 +0200

    SDI-TP1: Did a second time TD & TP with requested impl.

--- L2S4/SDI/TP1/src/dev/pimous/l2s4sdi/td1tp1/Liste.java
+++ L2S4/SDI/TP1/src/dev/pimous/l2s4sdi/td1tp1/Liste.java
@@ -9,10 +9,11 @@ public abstract sealed class Liste<E> implements Cloneable{
 	public abstract E getPremier();
 	public abstract Liste<E> getReste();
 
-	public long longueur(){
-		return estVide() ? 0 : 1 + getReste().longueur();
-	}
-	public long size(){ return longueur(); }
+	public abstract long longueur();
+	public abstract boolean trouver(E valeur);
+	public abstract boolean inclus(Liste<E> L);
+
+	public long size(){ return longueur(); };
 	public boolean recherche(E valeur){
 		Liste<E> noeud = this;
 
@@ -21,7 +22,6 @@ public abstract sealed class Liste<E> implements Cloneable{
 
 		return !noeud.estVide();
 	}
-	public boolean trouver(E valeur){ return recherche(valeur); }
 
 	// FUNCTIONS
 	public static <E> Liste<E> vide(){ return new ListeVide<E>(); }
@@ -29,43 +29,15 @@ public abstract sealed class Liste<E> implements Cloneable{
 		return new ListeCons<E>(valeur, suivant);
 	}
 
-	public Liste<E> inverser(){
-		return estVide() ? Liste.vide() :
-			getReste().inverser().placerFin(getPremier());
-	}
-	public Liste<E> inserer(E s, int rang){
-		return rang == 0 ? Liste.nouvelle(s, cloneListe())
-			: Liste.nouvelle(getPremier(), getReste().inserer(s, rang - 1));
-	}
+	public abstract Liste<E> inverser();
 
-	public Liste<E> placerFin(E valeur){
-		return estVide() ? Liste.nouvelle(valeur, Liste.vide())
-			: Liste.nouvelle(getPremier(), getReste().placerFin(valeur));
-	}
-	public Liste<E> concat(Liste<E> L1){
-		return estVide() ? L1.cloneListe()
-			: Liste.nouvelle(getPremier(), getReste().concat(L1));
-	}
-	public Liste<E> intersection(Liste<E> L){
-		return estVide() ? Liste.vide() : (
-			!L.trouver(getPremier()) ? getReste().intersection(L)
-				: Liste.nouvelle(getPremier(), getReste().intersection(L))
-		);
-	}
-	public Liste<E> union(Liste<E> L){
-		return estVide() ? L.cloneListe() : (
-			L.trouver(getPremier()) ? getReste().union(L)
-				: Liste.nouvelle(getPremier(), getReste().union(L))
-		);
-	}
+	public abstract Liste<E> inserer(E s, int rang);
+	public abstract Liste<E> placerFin(E valeur);
+	public abstract Liste<E> concat(Liste<E> L1);
+	public abstract Liste<E> intersection(Liste<E> L);
+	public abstract Liste<E> union(Liste<E> L);
 
-	public String somme(){
-		return getPremier() + getReste().somme();
-	}
-	public boolean inclus(Liste<E> L){
-		return L.estVide() ? true
-			: trouver(L.getPremier()) && inclus(L.getReste());
-	}
+	public abstract String somme();
 
 	public void afficheIter(PrintStream out){
 		StringBuilder sb = new StringBuilder(
@@ -81,20 +53,10 @@ public abstract sealed class Liste<E> implements Cloneable{
 		sb.replace(sb.length() - 2, sb.length(), "]");
 		out.println(sb);
 	}
-	public void affiche(PrintStream out){
-		out.printf("\t- %s\n", getPremier());
-		if(!getReste().estVide()) getReste().affiche(out);
-	}
-	public void afficheInverse(PrintStream out){
-		if(!getReste().estVide()) getReste().afficheInverse(out);
-		out.printf("\t- %s\n", getPremier());
-	}
+	public abstract void affiche(PrintStream out);
+	public abstract void afficheInverse(PrintStream out);
 
-	protected Liste<E> cloneListe(){
-		return estVide() ?
-			Liste.vide() :
-			Liste.nouvelle(getPremier(), getReste().cloneListe());
-	}
+	protected abstract Liste<E> cloneListe();
 	@Override
 	public Object clone(){ return cloneListe(); }
 
@@ -105,6 +67,41 @@ public abstract sealed class Liste<E> implements Cloneable{
 		public boolean estVide(){ return true; }
 		public E getPremier(){ return null; }
 		public Liste<E> getReste(){ return null; }
+
+		@Override
+		public long longueur(){ return 0; }
+		@Override
+		public boolean trouver(E valeur){ return false; }
+		@Override
+		public boolean inclus(Liste<E> L){ return L.estVide(); }
+
+		// FUNCTIONS
+		@Override
+		public Liste<E> inverser(){ return Liste.vide(); }
+
+		@Override
+		public Liste<E> inserer(E s, int rang){ return placerFin(s); }
+		@Override
+		public Liste<E> placerFin(E valeur){
+			return nouvelle(valeur, Liste.vide());
+		}
+		@Override
+		public Liste<E> concat(Liste<E> L1){ return L1.cloneListe(); }
+		@Override
+		public Liste<E> intersection(Liste<E> L){ return Liste.vide(); }
+		@Override
+		public Liste<E> union(Liste<E> L){ return L.cloneListe(); }
+
+		@Override
+		public String somme(){ return new String(); }
+
+		@Override
+		public void affiche(PrintStream out){ return; }
+		@Override
+		public void afficheInverse(PrintStream out){ return; }
+
+		@Override
+		protected Liste<E> cloneListe(){ return Liste.vide(); }
 	}
 
 	private static final class ListeCons<E> extends Liste<E>{
@@ -120,7 +117,73 @@ public abstract sealed class Liste<E> implements Cloneable{
 
 		// GETTERS
 		public boolean estVide(){ return false; }
-		public E getPremier(){ return this.valeur; }
-		public Liste<E> getReste(){ return this.suiv; }
+		public E getPremier(){ return valeur; }
+		public Liste<E> getReste(){ return suiv; }
+
+		@Override
+		public long longueur() {
+			return 1 + getReste().longueur();
+		}
+		@Override
+		public boolean trouver(E valeur) {
+			return valeur == this.valeur || getReste().trouver(valeur);
+		}
+		@Override
+		public boolean inclus(Liste<E> L){
+			return L.estVide() || (
+				trouver(L.getPremier()) && inclus(L.getReste())
+			);
+		}
+
+		// FUNCTIONS
+		@Override
+		public Liste<E> inverser(){
+			return getReste().inverser().placerFin(getPremier());
+		}
+
+		@Override
+		public Liste<E> inserer(E s, int rang){
+			return rang == 0 ? Liste.nouvelle(s, cloneListe())
+				: Liste.nouvelle(getPremier(), getReste().inserer(s, rang - 1));
+		}
+		@Override
+		public Liste<E> placerFin(E valeur){
+			return Liste.nouvelle(getPremier(), getReste().placerFin(valeur));
+		}
+		@Override
+		public Liste<E> concat(Liste<E> L1){
+			return Liste.nouvelle(getPremier(), getReste().concat(L1));
+		}
+		@Override
+		public Liste<E> intersection(Liste<E> L){
+			return !L.trouver(getPremier()) ? getReste().intersection(L)
+				: Liste.nouvelle(getPremier(), getReste().intersection(L));
+		}
+		@Override
+		public Liste<E> union(Liste<E> L){
+			return L.trouver(getPremier()) ? getReste().union(L)
+				: Liste.nouvelle(getPremier(), getReste().union(L));
+		}
+
+		@Override
+		public String somme(){
+			return getPremier() + getReste().somme();
+		}
+
+		@Override
+		public void affiche(PrintStream out){
+			out.printf("\t- %s\n", getPremier());
+			getReste().affiche(out);
+		}
+		@Override
+		public void afficheInverse(PrintStream out){
+			getReste().afficheInverse(out);
+			out.printf("\t- %s\n", getPremier());
+		}
+
+		@Override
+		protected Liste<E> cloneListe(){
+			return Liste.nouvelle(getPremier(), getReste().cloneListe());
+		}
 	}
 }
