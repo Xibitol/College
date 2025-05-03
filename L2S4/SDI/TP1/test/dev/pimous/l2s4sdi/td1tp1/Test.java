3f4eed3cd869bd0b650983146baafce2cd09be33
Xibitol <xibitol@pimous.dev>
2025-05-03 21:04:20 +0200

    SDI-TP1: Fixed my impl.

--- L2S4/SDI/TP1/test/dev/pimous/l2s4sdi/td1tp1/Test.java
+++ L2S4/SDI/TP1/test/dev/pimous/l2s4sdi/td1tp1/Test.java
@@ -9,22 +9,22 @@ class Test{
 	private Test(PrintStream out){
 		this.out = out;
 	}
-	
+
 	// FUNCTIONS
 	public static void main(String[] args){
 		Test context = new Test(System.out);
 
-		context.testTD();
-		context.testTP();
+		context.testBetterTD();
+		context.testBetterTP();
 	}
 
-	public void testTD(){
-		printsln("----------- TEST TD:");
+	public void testBetterTD(){
+		printsln("----------- TEST BETTER TD:");
 
-		Liste<String> nombres = Liste.nouvelle("Un",
-			Liste.nouvelle("Deux",
-				Liste.nouvelle("Trois",
-					Liste.nouvelle("Quatre", Liste.vide())
+		BetterListe<String> nombres = BetterListe.nouvelle("Un",
+			BetterListe.nouvelle("Deux",
+				BetterListe.nouvelle("Trois",
+					BetterListe.nouvelle("Quatre", BetterListe.vide())
 				)
 			)
 		);
@@ -50,22 +50,26 @@ class Test{
 
 		printsln("clone()");
 		Object cn = nombres.clone();
-		if(cn instanceof Liste ls) ls.afficheIter(out);
+		if(cn instanceof BetterListe ls) ls.afficheIter(out);
 
 		printsln("placerFin()");
 		nombres.placerFin("Cinq").afficheIter(out);
 	}
 
-	public void testTP(){
-		printsln("----------- TEST TP:");
+	public void testBetterTP(){
+		printsln("----------- TEST BETTER TP:");
+		
 
-		Liste<String> nombres = Liste.nouvelle("Un",
-			Liste.nouvelle("Deux",
-				Liste.nouvelle("Trois",
-					Liste.nouvelle("Quatre", Liste.vide())
+		BetterListe<String> nombres = BetterListe.nouvelle("Un",
+			BetterListe.nouvelle("Deux",
+				BetterListe.nouvelle("Trois",
+					BetterListe.nouvelle("Quatre", BetterListe.vide())
 				)
 			)
 		);
+
+		printsln("afficherIter()");
+		nombres.afficheIter(out);
 
 		printsln("trouver(\"Un\") ->", nombres.trouver("Un"));
 		printsln("trouver(\"Six\") ->", nombres.trouver("Six"));
@@ -76,45 +80,47 @@ class Test{
 		printsln("size() ->", nombres.size());
 
 		printsln("concat([Cinq, Six])");
-		Liste<String> nombresSuivant = Liste.nouvelle("Cinq",
-			Liste.nouvelle("Six", Liste.vide())
+		BetterListe<String> nombresSuivant = BetterListe.nouvelle("Cinq",
+			BetterListe.nouvelle("Six", BetterListe.vide())
 		);
 		nombres.concat(nombresSuivant).afficheIter(out);
 
 		printsln("concat([])");
-		nombres.concat(Liste.vide()).afficheIter(out);
+		nombres.concat(BetterListe.vide()).afficheIter(out);
 
 		printsln("inverser()");
 		nombres.inverser().afficheIter(out);
 
 		printsln("inserer(\"Deux et demi\", 2)");
 		nombres.inserer("Deux et demi", 2).afficheIter(out);
+		printsln("inserer(\"Six\", 5)");
+		nombres.inserer("Six", 5).afficheIter(out);
 
 		printsln("intersection([Un, Trois, Cinq])");
-		Liste<String> nombresIntersect = Liste.nouvelle("Un",
-			Liste.nouvelle("Trois",
-				Liste.nouvelle("Cinq", Liste.vide())
+		BetterListe<String> nombresIntersect = BetterListe.nouvelle("Un",
+			BetterListe.nouvelle("Trois",
+				BetterListe.nouvelle("Cinq", BetterListe.vide())
 			)
 		);
 		nombres.intersection(nombresIntersect).afficheIter(out);
 
-		printsln("intersection([Un, Trois, Cinq])");
-		Liste<String> nombresUnion = Liste.nouvelle("Un",
-			Liste.nouvelle("Trois",
-				Liste.nouvelle("Cinq", Liste.vide())
+		printsln("union([Un, Trois, Cinq])");
+		BetterListe<String> nombresUnion = BetterListe.nouvelle("Un",
+			BetterListe.nouvelle("Trois",
+				BetterListe.nouvelle("Cinq", BetterListe.vide())
 			)
 		);
 		nombres.union(nombresUnion).afficheIter(out);
 
-		Liste<String> nombresInclus = Liste.nouvelle("Deux",
-			Liste.nouvelle("Quatre", Liste.vide())
+		BetterListe<String> nombresInclus = BetterListe.nouvelle("Deux",
+			BetterListe.nouvelle("Quatre", BetterListe.vide())
 		);
 		printsln("inclus([Deux, Quatre]) ->",
 			nombres.inclus(nombresInclus)
 		);
 
-		nombresInclus = Liste.nouvelle("Deux",
-			Liste.nouvelle("Cinq", Liste.vide())
+		nombresInclus = BetterListe.nouvelle("Deux",
+			BetterListe.nouvelle("Cinq", BetterListe.vide())
 		);
 		printsln("inclus([Deux, Cinq]) ->",
 			nombres.inclus(nombresInclus)
