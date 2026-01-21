77e108bcf54964b8aa6f966657b8024e4d6033ba
Xibitol <xibitol@pimous.dev>
2026-01-21 20:25:27 +0100

    BDD-TP2: Finalized question 28.

--- I1S6/BDD/TP2/src/encapsulated.sql
+++ I1S6/BDD/TP2/src/encapsulated.sql
@@ -26,15 +26,13 @@ SELECT acteur.numero_acteur, acteur.nom_acteur FROM acteur
 	);
 
 -- 28 BIS (17:32 to 18:39 -> 1 heure et 7 minutes)
-SELECT acteur.numero_acteur, acteur.nom_acteur
-	FROM acteur
+SELECT acteur.numero_acteur, acteur.nom_acteur FROM acteur
 	INNER JOIN acteur AS aBis ON acteur.nation_acteur = aBis.nation_acteur
 	GROUP BY acteur.numero_acteur, acteur.nom_acteur
 	HAVING COUNT(aBis.numero_acteur) = (
-		SELECT COUNT(a2.numero_acteur) AS "count"
-			FROM acteur AS a1
-			INNER JOIN acteur AS a2 ON a1.nation_acteur = a2.nation_acteur
-			GROUP BY a1.numero_acteur
+		SELECT COUNT(a.numero_acteur) AS "count"
+			FROM acteur AS a
+			GROUP BY a.nation_acteur
 			ORDER BY "count" DESC
 			LIMIT 1
 	)
